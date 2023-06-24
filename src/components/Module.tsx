6be116c0f79f8ef9fe2735de84e4cd73f8f12de3
marlisonmourao <bentesmourao@gmail.com>
2023-06-24 14:45:26 -0400

    refactor: Sepando a interface em components

---
+++ src/components/Module.tsx
@@ -0,0 +1,36 @@
+import { ChevronDown } from 'lucide-react'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Lesson } from './Lesson'
+
+interface ModuleProps {
+  title: string
+  amountOfLessons: number
+  moduleIndex: number
+}
+
+export function Module({ amountOfLessons, title, moduleIndex }: ModuleProps) {
+  return (
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
+        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-sm">
+          {moduleIndex + 1}
+        </div>
+
+        <div className="flex flex-col gap-1 text-left">
+          <strong className="text-sm">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
+        </div>
+
+        <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 transition-transform group-data-[state=open]:rotate-180" />
+      </Collapsible.Trigger>
+
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-4">
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  )
+}
